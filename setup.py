3ff9fa33969e76f717cd566434333532d493d05f
Chris Streeter <chris@chrisstreeter.com>
2014-03-01 07:51:15 -0600

    Add publish shortcut to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import sys
 from setuptools import setup, find_packages
 
 import activity_tracker
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register sdist bdist_wheel upload')
+    sys.exit()
+
 
 setup(
     name='activity-tracker',
